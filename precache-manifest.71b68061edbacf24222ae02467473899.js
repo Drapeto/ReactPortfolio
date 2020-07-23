9642f7a5fb62736f5228af0a7939f5dfd8c2c104
Drapeto <drapetorres@gmail.com>
2020-07-22 23:42:52 -0700

    Updates

---
+++ precache-manifest.71b68061edbacf24222ae02467473899.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e71b26c2601be243010db6d681408f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7582b7dd26dca7bfc1c",
+    "url": "/static/js/main.c936dfd3.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
